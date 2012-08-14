da7e3900136909e9917ee28911b33cd1dee24198
Brett Dargan <brett.dargan@gmail.com>
2012-08-14 17:04:08 +1000

    mods

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -2,8 +2,8 @@
 #unamestr = 'uname'
 
 if [[ $('uname') == 'Linux' ]]; then
-	alias st='/usr/bin/sublime_text&'
+	alias sl='/usr/bin/sublime_text&'
 elif  [[ $('uname') == 'Darwin' ]]; then
-	alias st='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+	alias sl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
 fi
-alias stt='st .'
+alias sll='sl .'
