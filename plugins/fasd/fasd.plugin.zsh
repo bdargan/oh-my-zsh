51983ff9048ac5a35efbbabb0187667ef4ee6dc8
Brett <brett@brett-msi>
2012-08-18 22:28:16 +1000

    use j fasd alias

--- plugins/fasd/fasd.plugin.zsh
+++ plugins/fasd/fasd.plugin.zsh
@@ -2,5 +2,12 @@ if [ $commands[fasd] ]; then # check if fasd is installed
   eval "$(fasd --init auto)"
   alias v='f -e vim'
   alias o='a -e open'
+  alias j='fasd_cd -d'
+  alias jj='fasd_cd -di'
+
+  #undo alias 
+  unalias z
+  unalias zz
+
 fi
 
